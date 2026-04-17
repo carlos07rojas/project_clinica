b8957ebf84d3d332ef6999575271533ec10f7427
carlos07rojas <raulcjm07@gmail.com>
2026-04-17 12:56:28 -0500

    Se agrego Service y DTO

--- backend/src/main/java/com/projectclinica/backend/service/CitaService.java
+++ backend/src/main/java/com/projectclinica/backend/service/CitaService.java
@@ -1,23 +1,151 @@
 package com.projectclinica.backend.service;
 
-// import com.projectclinica.backend.dto.CitaRequestDTO;
-// import com.projectclinica.backend.dto.CitaResponseDTO;
-// import com.projectclinica.backend.model.Cita;
-// import com.projectclinica.backend.model.Medico;
-// import com.projectclinica.backend.model.Paciente;
-// import com.projectclinica.backend.model.Servicio;
-// import com.projectclinica.backend.repository.CitaRepository;
-// import com.projectclinica.backend.repository.HorarioCitasRepository;
-// import com.projectclinica.backend.repository.MedicoRepository;
-// import com.projectclinica.backend.repository.PacienteRepository;
-// import com.projectclinica.backend.repository.ServicioRepository;
+import com.projectclinica.backend.dto.CitaRequestDTO;
+import com.projectclinica.backend.dto.CitaResponseDTO;
+import com.projectclinica.backend.model.Cita;
+import com.projectclinica.backend.model.Medico;
+import com.projectclinica.backend.model.Paciente;
+import com.projectclinica.backend.model.Servicio;
+import com.projectclinica.backend.repository.CitaRepository;
+import com.projectclinica.backend.repository.HorarioCitasRepository;
+import com.projectclinica.backend.repository.MedicoRepository;
+import com.projectclinica.backend.repository.PacienteRepository;
+import com.projectclinica.backend.repository.ServicioRepository;
 import org.springframework.stereotype.Service;
 // import java.time.DayOfWeek;
-// import java.time.LocalDateTime;
-// import java.util.List;
+import java.time.LocalDateTime;
+import java.util.List;
 // import java.util.stream.Collectors;
 
 @Service
 public class CitaService {
+    private final CitaRepository citaRepository;
+    private final HorarioCitasRepository horarioCitasRepository;
+    private final MedicoRepository medicoRepository;
+    private final PacienteRepository pacienteRepository;
+    private final ServicioRepository servicioRepository;
+
+    public CitaService(CitaRepository citaRepository,
+            HorarioCitasRepository horarioCitasRepository,
+            MedicoRepository medicoRepository,
+            PacienteRepository pacienteRepository,
+            ServicioRepository servicioRepository) {
+        this.citaRepository = citaRepository;
+        this.horarioCitasRepository = horarioCitasRepository;
+        this.medicoRepository = medicoRepository;
+        this.pacienteRepository = pacienteRepository;
+        this.servicioRepository = servicioRepository;
+    }
+    
+    // agrega citas
+    public CitaResponseDTO agendarCita(CitaRequestDTO dto) {
+        // para esto el paciente debe existir
+        Paciente paciente = pacienteRepository.findById(dto.getIdPaciente()).orElseThrow(() -> new RuntimeException(
+            "Paciente no encontrado con id: " + dto.getIdPaciente()
+        ));
+
+        // para esto el medico debe de existir
+        Medico medico = medicoRepository.findById(dto.getIdMedico()).orElseThrow(() -> new RuntimeException(
+            "Medico no encontrado con id: " + dto.getIdMedico()
+        ));
+
+        // el medico debe estar activo en la clinica
+        if (!medico.getUsuario().getActivo()) {
+            throw new RuntimeException(
+                    "El medico no esta activo");
+        }
+        
+        // el servicio debe existir y estar activo
+        Servicio servicio = servicioRepository.findById(dto.getIdServicio()).orElseThrow(() -> new RuntimeException(
+            "El servicio " + dto.getIdServicio() + " no fue encontrado"
+        ));
+        if (!servicio.getActivo()) {
+            throw new RuntimeException(
+                    "El servicio no esta activo");
+        }
+        
+        // el servicio debe pertenecer a una especialidad de un medico
+        if (!servicio.getEspecialidad().getIdEspecialidad().equals(medico.getEspecialidad().getIdEspecialidad())) {
+            throw new RuntimeException(
+                    "El servicio no corresponde al servicio del medico");
+        }
+        
+        // la cita debe agendarse con 24 horas de anticipacion
+        LocalDateTime ahora = LocalDateTime.now();
+        LocalDateTime minimoPermitido = ahora.plusHours(24);
+        if (dto.getFechaHora().isBefore(minimoPermitido)) {
+            throw new RuntimeException(
+                    "La cita debe agendarse con al menos 24 horas de anticipacion");
+        }
+        
+        //el medico debe trabajar ese dia de la semana
+        int diaSemana = dto.getFechaHora().getDayOfWeek().getValue();
+        var horariosDelDia = horarioCitasRepository.findHorariosActivosPorMedicoYDia(dto.getIdMedico(), diaSemana);
+        if (horariosDelDia.isEmpty()) {
+            throw new RuntimeException(
+                    "El medico no trabaja ese dia de la semana");
+        }
+        
+        // la hora de la cita debe estar dentro del rango del horario laboral del medico en ese dia
+        var horario = horariosDelDia.get(0);
+        var horaCita = dto.getFechaHora().toLocalTime();
+        if (horaCita.isBefore(horario.getHoraInicio()) || 
+                horaCita.isAfter(horario.getHorarioFin())) {
+            throw new RuntimeException(
+                    "La hora esta fuera del horario del medico. Trabaja de " + horario.getHoraInicio() + " a "
+                            + horario.getHorarioFin());
+        }
+        // el medico no debe tener otra cita en la misma hora
+        if (citaRepository.existeCitaEnHorario(dto.getIdMedico(), dto.getFechaHora())) {
+            throw new RuntimeException(
+                    "El medico no esta disponible para esa hora");
+        }
+        
+        // el paciente no debe tener una cita en la misma hora
+        List<Cita> citasPaciente = citaRepository.findByPacienteId(dto.getIdPaciente());
+        boolean pacienteTieneCitaEsaHora = citasPaciente.stream()
+                .anyMatch(c -> c.getFechaHora().equals(dto.getFechaHora()) && !c.getEstado().equals("CANCELADA"));
+        if (pacienteTieneCitaEsaHora) {
+            throw new RuntimeException(
+                    "El paciente ya tiene una cita en ese horario");
+        }
+        
+        // el paciente no puede tener mas de 3 citas pendientes al mismo tiempo 
+        long citasPendientes = citasPaciente.stream().filter(c -> c.getEstado().equals("PENDIENTE")).count();
+        if (citasPendientes >= 3) {
+            throw new RuntimeException(
+                    "El paciente ya tiene 3 citas pendientes. Debe confirmar o cancelar alguna antes de agendar otra");
+        }
+        
+        Cita cita = new Cita();
+        cita.setPaciente(paciente);
+        cita.setMedico(medico);
+        cita.setServicio(servicio);
+        cita.setFechaHora(dto.getFechaHora());
+        cita.setDuracionMin(servicio.getDuracionMin());
+        // usamos la duración del servicio directamente para que sea consistente con lo que ofrece la clínica
+        cita.setObservaciones(dto.getObservaciones());
+
+        Cita guardada = citaRepository.save(cita);
+        return convertirAReponseDTO(guardada);
+
+    }
+    
+    private CitaResponseDTO convertirAReponseDTO(Cita c) {
+        CitaResponseDTO dto = new CitaResponseDTO();
+        dto.setIdCita(c.getIdCita());
+        dto.setIdPaciente(c.getPaciente().getIdPaciente());
+        dto.setNombrePaciente(c.getPaciente().getUsuario().getNombre() + " " + c.getMedico().getUsuario().getNombre());
+        dto.setIdMedico(c.getMedico().getIdMedico());
+        dto.setNombreMedico(c.getMedico().getUsuario().getNombre() + " " + c.getMedico().getUsuario().getApellido());
+        dto.setIdServicio(c.getServicio().getIdServicio());
+        dto.setNombreServicio(c.getServicio().getNombre());
+        dto.setFechaHora(c.getFechaHora());
+        dto.setDuracionMin(c.getDuracionMin());
+        dto.setEstado(c.getEstado());
+        dto.setObservacion(c.getObservaciones());
+        dto.setFechaCreacion(c.getFechaCreacion());
+        return dto;
+    }    
     
 }
