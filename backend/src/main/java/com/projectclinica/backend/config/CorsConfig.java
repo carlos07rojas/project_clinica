dc6103d82935e153b5d2a165f4008fe986517e4e
carlos07rojas <raulcjm07@gmail.com>
2026-06-16 23:54:49 -0500

    se agrega masAge(3600) para hacer que el navegador no repita el preflight y hace que mejore el rendimiento

--- backend/src/main/java/com/projectclinica/backend/config/CorsConfig.java
+++ backend/src/main/java/com/projectclinica/backend/config/CorsConfig.java
@@ -5,24 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
 @Configuration // le dice a spring que esta clase define configuraciones del sistema
 public class CorsConfig {
-   @Bean
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                    // permite peticiones desde Angular
-                    .allowedOrigins("http://localhost:4200")
-                    // permite todos los métodos HTTP
-                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                    // permite todos los headers
-                    .allowedHeaders("*")
-                    // permite credenciales
-                    .allowCredentials(true);
+                        // permite peticiones desde Angular
+                        .allowedOrigins("http://localhost:4200")
+                        // permite todos los métodos HTTP
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        // permite todos los headers
+                        .allowedHeaders("*")
+                        // permite credenciales
+                        .allowCredentials(true)
+                        // cachea el preflight 1 hora
+                        .maxAge(3600);
             }
         };
     }
